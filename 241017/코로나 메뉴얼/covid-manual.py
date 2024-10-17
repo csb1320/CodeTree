a90aee50f87d3374a8b8daeb152b8713f922a3b8
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-17 22:34:27 +0900

    Title: 코로나 메뉴얼, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/코로나 메뉴얼/covid-manual.py
+++ 241017/코로나 메뉴얼/covid-manual.py
@@ -2,43 +2,13 @@ a = input().split()
 b = input().split()
 c = input().split()
 
-if a[0] == 'Y' and b[0] =='Y' and c[0] == 'Y':
-    if int(a[1]) >= 37:
-        if int(b[1] >= 37):
-            if int(c[1] >= 37):
-                print('E')
-            else:
-                print('E')
-        elif int(c[1] >= 37):
-            if int(b[1] >= 37):
-                print('E')
-            else:
-                print('E')
-        else:
-            print('E')
-    else:
+if a[0] == 'Y' and int(a[1]) >= 37:
+    if(b[0] == 'Y' and int(b[1]) >= 37) or (c[0] == 'Y' and int(c[1]) >= 37):
         print('E')
-
-if a[0] == 'Y':
-    if b[0] == 'Y':
-        if int(a[1]) >= 37 and int(b[1]) >= 37:
-            print('E')
-        else:
-            print('N')
-    elif c[0] == 'Y':
-        if int(a[1]) >= 37 and int(c[1]) >= 37:
-            print('E')
-        else:
-            print('N')
-    else:
-        print('N')
-elif b[0] == 'Y':
-    if c[0] == 'Y':
-        if int(b[1]) >= 37 and int(c[1]) >= 37:
-            print('E')
-        else:
-            print('N')
     else:
         print('N')
 else:
-    print('N')
+    if(b[0] == 'Y' and int(b[1]) >= 37) and (c[0] == 'Y' and int(c[1]) >= 37):
+        print('E')
+    else:
+        print('N')
