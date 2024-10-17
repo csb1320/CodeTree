9a16d21dffb11cc24cd69a2da8e32240605eeb5e
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-17 10:54:24 +0900

    Title: 합과 평균의 차, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/합과 평균의 차/sub-of-average-and-sum.py
+++ 241017/합과 평균의 차/sub-of-average-and-sum.py
@@ -2,12 +2,7 @@ a, b, c = map(int, input().split())
 
 arr = [a, b, c]
 
-if(sum(arr) % 3 == 0):
-    print(sum(arr))
-    print(sum(arr)//len(arr))
-    print(sum(arr) - sum(arr)//len(arr))
 
-else:
-    print(sum(arr))
-    print(sum(arr)/len(arr))
-    print(sum(arr) - sum(arr)/len(arr))
+print(sum(arr))
+print(sum(arr)//len(arr))
+print(sum(arr) - sum(arr)//len(arr))
