37a705742a2fb15f13d2d48b7d90b37ac134860b
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-17 22:30:28 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241017/코로나 메뉴얼/covid-manual.py
+++ 241017/코로나 메뉴얼/covid-manual.py
@@ -2,6 +2,23 @@ a = input().split()
 b = input().split()
 c = input().split()
 
+if a[0] == 'Y' and b[0] =='Y' and c[0] == 'Y':
+    if int(a[1]) >= 37:
+        if int(b[1] >= 37):
+            if int(c[1] >= 37):
+                print('E')
+            else:
+                print('E')
+        elif int(c[1] >= 37):
+            if int(b[1] >= 37):
+                print('E')
+            else:
+                print('E')
+        else:
+            print('E')
+    else:
+        print('E')
+
 if a[0] == 'Y':
     if b[0] == 'Y':
         if int(a[1]) >= 37 and int(b[1]) >= 37:
