352de5fca23b361e9b1d8326bc2e77ffc0a6ae93
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-17 20:55:28 +0900

    Title: 장학금, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241017/장학금/scholarship.py
@@ -0,0 +1,11 @@
+mid_te, fin_te = map(int, input().split())
+
+if mid_te >= 90:
+    if fin_te >= 95:
+        print(100000)
+    elif fin_te >= 90:
+        print(50000)
+    else:
+        print(0)
+else:
+    print(0)
