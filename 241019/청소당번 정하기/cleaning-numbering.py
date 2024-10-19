24cd9c7bff7c21313e97e833a958e86b8d776cb6
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-19 17:43:22 +0900

    Title: 청소당번 정하기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 241019/청소당번 정하기/cleaning-numbering.py
+++ 241019/청소당번 정하기/cleaning-numbering.py
@@ -5,18 +5,29 @@ pass_cnt = 0
 toilet_cnt = 0
 
 for i in range(1, n + 1):
-    if i % 2 == 0:
-        if i % 3 == 0:
-            if i % 12 == 0:
-                toilet_cnt += 1
-            else:
-                pass_cnt += 1
-        else:
-            class_cnt += 1
+    # 주기가 가장 긴 12일부터 확인합니다.
+    if i % 12 == 0:
+        toilet_cnt += 1
+    # 12일 주기에 들어오지 않는다면, 3일 주기에 들어오는지 확인합니다.
     elif i % 3 == 0:
-        if i % 12 == 0:
-            toilet_cnt += 1
-        else:
-            pass_cnt += 1
+        pass_cnt += 1
+    # 3일 주기에도 들어오지 않는다면, 2일 주기에 들어오는지 확인합니다.
+    elif i % 2 == 0:
+        class_cnt += 1
+
+# for i in range(1, n + 1):
+#     if i % 2 == 0:
+#         if i % 3 == 0:
+#             if i % 12 == 0:
+#                 toilet_cnt += 1
+#             else:
+#                 pass_cnt += 1
+#         else:
+#             class_cnt += 1
+#     elif i % 3 == 0:
+#         if i % 12 == 0:
+#             toilet_cnt += 1
+#         else:
+#             pass_cnt += 1
 
 print(class_cnt, pass_cnt, toilet_cnt)
