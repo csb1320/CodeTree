a6fd9a470eee16853562ba326b128f2a29d64713
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-19 17:20:04 +0900

    Title: 369 게임, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241019/369 게임/369-game.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    if i % 3 == 0 or str(i).count('3') or str(i).count('6')or str(i).count('9'):
+        print(0, end = " ")
+    else:
+        print(i, end = " ")
