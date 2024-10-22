b5da08e6a2de7daf7050e869e52a86ff73148672
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-10-22 21:02:25 +0900

    Title: 2의 거듭제곱수, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/2의 거듭제곱수/pow-of-2.py
+++ 241022/2의 거듭제곱수/pow-of-2.py
@@ -1,8 +1,16 @@
 N = int(input())
+prod = 1
+x = 0
 
+while True:
+	# prod가 n이 될 때까지 2를 곱합니다.
+	if N == prod:
+		break
 
-for i in range(11):
-    if N // (2 ** i) == 1:
-        break
+	prod *= 2
+	x += 1
+# for i in range(11):
+#     if N // (2 ** i) == 1:
+#         break
     
-print(i)
+print(x)
